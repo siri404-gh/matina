af9bb61bb5398de5dbea371e12cb99de53eaf722
Sreeram Padmanabhan <sreeram.com@gmail.com>
2018-12-07 18:10:42 +0000

    refactor: update

--- config/variables.js
+++ config/variables.js
@@ -54,11 +54,11 @@ module.exports = {
   dist: '_dist',
   firebaseConfig: {
     apiKey: process.env.FIREBASE_API_KEY,
-    authDomain: "sreeram-stage.firebaseapp.com",
-    databaseURL: "https://sreeram-stage.firebaseio.com",
-    projectId: "sreeram-stage",
-    storageBucket: "sreeram-stage.appspot.com",
-    messagingSenderId: "857242952614",
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    databaseURL: process.env.FIREBASE_DATABASE_URL,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
   },
   adsensePubId: 'ca-pub-6831276331714408',
 };
--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -29,6 +29,11 @@ module.exports = merge(commonConfig, {
       'process.env': {
         NODE_ENV: JSON.stringify('development'),
         FIREBASE_API_KEY: JSON.stringify(process.env.FIREBASE_API_KEY_STAGE),
+        FIREBASE_AUTH_DOMAIN: JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN_STAGE),
+        FIREBASE_DATABASE_URL: JSON.stringify(process.env.FIREBASE_DATABASE_URL_STAGE),
+        FIREBASE_PROJECT_ID: JSON.stringify(process.env.FIREBASE_PROJECT_ID_STAGE),
+        FIREBASE_STORAGE_BUCKET: JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET_STAGE),
+        FIREBASE_MESSAGING_SENDER_ID: JSON.stringify(process.env.FIREBASE_MESSAGING_SENDER_ID_STAGE),
       },
     }),
   ],
